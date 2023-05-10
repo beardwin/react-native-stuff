eedc658eb9c265e2859b46e2bcc753788cb65867
Ryan Baldwin <ryanbaldwin+beardwingithub@gmail.com>
2023-05-09 21:21:38 -0400

    more cleaning

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs>
       <Tabs.Screen name="components" options={{ title: "Components" }} />
       <Tabs.Screen name="navigation" options={{ title: "Navigation" }} />
     </Tabs>
